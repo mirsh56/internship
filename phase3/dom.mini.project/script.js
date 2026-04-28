8131b1fda2a8a4c91619d5a784b65a25677d45de
mirsh56 <nishajamirshy@gmail.com>
2026-04-29 00:44:40 +0530

    Add files via upload

---
+++ phase3/dom.mini.project/script.js
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>DOM Project Work</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+
+  <h1>JavaScript DOM Project</h1>
+  <h3>Name: FRIEND_NAME_HERE</h3>
+
+  <h4>Item List</h4>
+  <input id="itemInput" placeholder="Type item name">
+  <button id="addButton">Insert</button>
+  <ul id="itemList"></ul>
+
+  <hr>
+
+  <h4>Task Manager</h4>
+  <input id="taskInput" placeholder="Type new task">
+  <button id="taskButton">Create Task</button>
+  <ul id="taskList"></ul>
+
+  <hr>
+
+  <h4>Visibility Control</h4>
+  <p id="msgText">Welcome to My Project</p>
+  <button id="showBtn">Toggle View</button>
+
+  <script src="script.js"></script>
+</body>
+</html>
